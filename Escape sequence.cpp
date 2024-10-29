19116f44714b1df00c68db28a1b8c6a571edf217
Prubdeep <93209148+Prubdeep@users.noreply.github.com>
2024-10-29 18:30:21 +0300

    Add files via upload

---
+++ Escape sequence.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "*****Escape Sequence Practice*****\n";
+    std::cout << "BBIT is a university diplome designed to focus on:\n";
+    std::cout << "  1. Development \n";
+    std::cout << "  2. Implementation and, \n";
+    std::cout << "  1. Management of Information Systems ";
+}
